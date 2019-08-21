eb5897c88c6f988177c544f4443b49acd3a3b59d
Abhinav Tomar <atomar.cse18@chitkarauniversity.edu.in>
2019-08-21 19:33:09 +0530

    Query 1 works and fixed a typo causing errors

--- log-analyzer.py
+++ log-analyzer.py
@@ -10,16 +10,14 @@ def send_query(query):
     return c.fetchall()
     db.close()
 //Find top 3 posts of all time
-q1='''select title,count(*) as views
-      from articles,log
-      where log.path=CONCAT('/articles/',articles.slug)
-      group by articles.title
-      order by views desc'''
+q1='''select title,count(*) as views from articles inner join 
+      log on CONCAT('/article/',articles.slug)=log.path
+      where log.status='200 OK'
+      group by log.path,articles.title order by views desc limit 3;'''
 //Views on posts by different authors
-q2='''select authors.name,count(*) as views
-      from articles inner join authors
-      on articles.author = authors.id
-      inner join log 
-      on CONCAT('/articles/',articles.slug)'''
+q2='''select authors.name,count(*) as views from articles inner join 
+      authors on articles.author = authors.id inner join 
+      log on CONCAT('/article/',articles.slug)=log.path where 
+      log.status='200 OK' group by authors.name order by views desc;'''
 //Find Days having more than 1 percent downtime
 q3='''//QUERY 3'''
